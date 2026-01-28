8160aa44a443d1f63e6ce12cbf4eb70990f9d9df
pokhrelashok2 <pokhrelashok2@gmail.com>
2026-01-28 10:57:53 +0545

    FInal fix

--- migrations/2026_01_28_000001_add_unique_constraint_to_stock_prices.sql
+++ migrations/2026_01_28_000001_add_unique_constraint_to_stock_prices.sql
@@ -1,8 +1,8 @@
 -- Add unique constraint to stock_prices to prevent duplicates
 -- Add index on business_date to speed up queries and cleanup
-CREATE INDEX IF NOT EXISTS idx_stock_prices_date ON stock_prices (business_date);
+CREATE INDEX idx_stock_prices_date ON stock_prices (business_date);
 
 -- Add the unique constraint on (symbol, business_date)
-CREATE UNIQUE INDEX IF NOT EXISTS unique_symbol_date ON stock_prices (symbol, business_date);
+CREATE UNIQUE INDEX unique_symbol_date ON stock_prices (symbol, business_date);
 
 
