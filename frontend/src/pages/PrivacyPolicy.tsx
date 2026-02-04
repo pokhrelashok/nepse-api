f3fab6e6e9ad6b38f75dfc74da0eb153facd69c4
pokhrelashok2 <pokhrelashok2@gmail.com>
2026-02-04 10:29:40 +0545

    Deployment script added

--- frontend/src/pages/PrivacyPolicy.tsx
+++ frontend/src/pages/PrivacyPolicy.tsx
@@ -84,8 +84,30 @@ export default function PrivacyPolicyPage() {
           <p style={styles.p}>If you have questions, contact us at <a href="mailto:portfolionepse@gmail.com" style={styles.link}>portfolionepse@gmail.com</a>.</p>
 
           <h2 style={styles.h2}>10. Advertising Disclosures</h2>
-          <p style={styles.p}>We use Ezoic to provide personalization and analytic services on this website, as such Ezoic's privacy policy is in effect and can be reviewed here.</p>
-          <span id="ezoic-privacy-policy-embed"></span>
+          <h3 style={{ ...styles.h2, fontSize: 18, marginTop: 20 }}>Ezoic Services</h3>
+          <p style={styles.p}>
+            This website uses the services of Ezoic Inc. (“Ezoic”), including to manage third-party interest-based advertising. Ezoic may employ a variety of technologies on this website, including tools to serve content, display advertisements and enable advertising to visitors of this website, which may utilize first and third-party cookies.
+          </p>
+          <p style={styles.p}>
+            A cookie is a small text file sent to your device by a web server that enables the website to remember information about your browsing activity. First-party cookies are created by the site you are visiting, while third-party cookies are set by domains other than the one you're visiting. Ezoic and our partners may place third-party cookies, tags, beacons, pixels, and similar technologies to monitor interactions with advertisements and optimize ad targeting. Please note that disabling cookies may limit access to certain content and features on the website, and rejecting cookies does not eliminate advertisements but will result in non-personalized advertising. You can find more information about cookies and how to manage them <a href="https://allaboutcookies.org/" target="_blank" rel="noreferrer" style={styles.link}>here</a>.
+          </p>
+          <p style={styles.p}>
+            The following information may be collected, used, and stored in a cookie when serving personalized ads:
+          </p>
+          <ul style={styles.ul}>
+            <li style={styles.li}>IP address</li>
+            <li style={styles.li}>Operating system type and version</li>
+            <li style={styles.li}>Device type</li>
+            <li style={styles.li}>Language preferences</li>
+            <li style={styles.li}>Web browser type</li>
+            <li style={styles.li}>Email (in a hashed or encrypted form)</li>
+          </ul>
+          <p style={styles.p}>
+            Ezoic and its partners may use this data in combination with information that has been independently collected to deliver targeted advertisements across various platforms and websites. Ezoic’s partners may also gather additional data, such as unique IDs, advertising IDs, geolocation data, usage data, device information, traffic data, referral sources, and interactions between users and websites or advertisements, to create audience segments for targeted advertising across different devices, browsers, and apps. You can find more information about interest-based advertising and how to manage them <a href="https://youradchoices.com/" target="_blank" rel="noreferrer" style={styles.link}>here</a>.
+          </p>
+          <p style={styles.p}>
+            You can view Ezoic’s privacy policy <a href="https://ezoic.com/privacy/" target="_blank" rel="noreferrer" style={styles.link}>here</a>, or for additional information about Ezoic’s advertising and other partners, you can view Ezoic’s advertising partners <a href="https://www.ezoic.com/privacy-policy/advertising-partners/" target="_blank" rel="noreferrer" style={styles.link}>here</a>.
+          </p>
         </div>
       </div>
     </div>
